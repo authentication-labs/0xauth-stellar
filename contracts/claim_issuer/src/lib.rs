b3c4235e4172c2738391ac217b8c7989c5614de1
Hamza Khalid <hamzakhalid829@gmail.com>
2024-06-03 10:41:40 +0500

    Add Error messages to claim issuer

--- contracts/claim_issuer/src/lib.rs
+++ contracts/claim_issuer/src/lib.rs
@@ -16,7 +16,7 @@ mod claim_issuer {
 }
 
 mod state;
-use state::{Claim, Key, KeyPurpose, KeyType};
+use state::{Claim, Key, KeyPurpose, KeyType, Error};
 
 
 #[contract]
@@ -24,14 +24,15 @@ pub struct ClaimIssuerContract;
 
 #[contractimpl]
 impl ClaimIssuerContract {
-    pub fn get_initialized(env: Env) -> bool {
-        env.storage()
+    pub fn get_initialized(env: Env) -> Result<bool, Error> {
+        Ok(env
+            .storage()
             .instance()
             .get::<Symbol, bool>(&Symbol::new(&env, "initialized"))
-            .unwrap_or(false)
+            .unwrap_or(false))
     }
 
-    pub fn initialize(env: Env, initial_management_key: Address) {
+    pub fn initialize(env: Env, initial_management_key: Address) -> Result<(), Error> {
         initial_management_key.require_auth();
 
         let initialized = env
@@ -41,7 +42,7 @@ impl ClaimIssuerContract {
             .unwrap_or(false);
 
         if initialized {
-            panic!("Contract already initialized");
+            return Err(Error::AlreadyInitialized);
         }
         env.storage()
             .instance()
@@ -66,12 +67,13 @@ impl ClaimIssuerContract {
 
         log!(
             &env,
-            "claim issuer identity contract initialized with management key: {:?}",
+            "Identity contract initialized with management key: {:?}",
             initial_management_key
         );
+        Ok(())
     }
 
-    pub fn get_key(env: Env, key: Address) -> Key {
+    pub fn get_key(env: Env, key: Address) -> Result<Key, Error> {
         let key_hash = hash_key(&env, &key);
         let keys = env
             .storage()
@@ -81,24 +83,24 @@ impl ClaimIssuerContract {
 
         keys.iter()
             .find(|k| k.key == key_hash)
-            .expect("Key not found")
             .clone()
+            .ok_or(Error::KeyNotFound)
     }
 
-    pub fn get_keys(env: Env) -> Vec<Key> {
-        env.storage()
+    pub fn get_keys(env: Env) -> Result<Vec<Key>, Error> {
+        Ok(env.storage()
             .persistent()
             .get::<Symbol, Vec<Key>>(&symbol_short!("keys"))
-            .unwrap_or(Vec::new(&env))
+            .unwrap_or(Vec::new(&env)))
     }
 
-    pub fn add_key(env: Env, manager: Address, key: Address, purpose: u32, key_type: u32) {
+    pub fn add_key(env: Env, manager: Address, key: Address, purpose: u32, key_type: u32) -> Result<(), Error> {
         // Only the manager can add keys
-        identity_require_auth(&env, &manager, KeyPurpose::Management);
+        identity_require_auth(&env, &manager, KeyPurpose::Management)?;
 
         // Make a Kecak256 hash of the key
         let key_hash = hash_key(&env, &key);
-        let key_purpose = KeyPurpose::try_from(purpose).expect("Invalid key purpose");
+        let key_purpose = KeyPurpose::try_from(purpose).map_err(|_| Error::InvalidKeyPurpose)?;
 
         // Retrieve and mutate the list of keys
         let mut keys: Vec<Key> = env
@@ -110,10 +112,10 @@ impl ClaimIssuerContract {
         let mut key_found = false;
 
         for i in 0..keys.len() {
-            let mut k = keys.get(i).expect("Index out of bounds");
+            let mut k = keys.get(i).ok_or(Error::IndexOutOfBounds)?;
             if k.key == key_hash {
                 if k.purposes.contains(&key_purpose) {
-                    panic!("Conflict: Key already exists with the same purpose");
+                    return Err(Error::KeyConflict);
                 } else {
                     k.purposes.push_back(key_purpose);
                     key_found = true;
@@ -125,7 +127,7 @@ impl ClaimIssuerContract {
         if !key_found {
             let key = Key {
                 purposes: vec![&env, key_purpose],
-                key_type: KeyType::try_from(key_type).expect("Invalid key type"),
+                key_type: KeyType::try_from(key_type).map_err(|_| Error::InvalidKeyType)?,
                 key: key_hash.clone(),
             };
             keys.push_back(key);
@@ -135,15 +137,16 @@ impl ClaimIssuerContract {
             .set(&symbol_short!("keys"), &keys);
 
         // TODO: Emit Key Add Event
+        Ok(())
     }
 
-    pub fn remove_key(env: Env, manager: Address, key: Address, purpose: u32) {
+    pub fn remove_key(env: Env, manager: Address, key: Address, purpose: u32) -> Result<(), Error> {
         // Only the manager can remove keys
-        identity_require_auth(&env, &manager, KeyPurpose::Management);
+        identity_require_auth(&env, &manager, KeyPurpose::Management)?;
 
         // Make a Kecak256 hash of the key
         let key_hash = hash_key(&env, &key);
-        let key_purpose = KeyPurpose::try_from(purpose).expect("Invalid key purpose");
+        let key_purpose = KeyPurpose::try_from(purpose).map_err(|_| Error::InvalidKeyPurpose)?;
 
         // Retrieve and mutate the list of keys
         let mut keys: Vec<Key> = env
@@ -152,17 +155,17 @@ impl ClaimIssuerContract {
             .get::<Symbol, Vec<Key>>(&symbol_short!("keys"))
             .unwrap_or(Vec::new(&env));
 
-        if !keys.iter().any(|k| k.key == key_hash) {
-            panic!("Key not found");
-        }
+            if !keys.iter().any(|k| k.key == key_hash) {
+                return Err(Error::KeyNotFound);
+            }
 
         for i in 0..keys.len() {
-            let mut k = keys.get(i).expect("Index out of bounds");
+            let mut k = keys.get(i).ok_or(Error::IndexOutOfBounds)?;
             if k.key == key_hash {
                 if let Some(pos) = k.purposes.iter().position(|p| p == key_purpose) {
                     k.purposes.remove(pos as u32);
                 } else {
-                    panic!("Key does not have the specified purpose");
+                    return Err(Error::KeyDoesNotHavePurpose);
                 }
 
                 if k.purposes.is_empty() {
@@ -175,21 +178,21 @@ impl ClaimIssuerContract {
             .persistent()
             .set(&symbol_short!("keys"), &keys);
         // TODO: Emit Key Removed Event
+        Ok(())
     }
 
-    pub fn get_claim(env: Env, claim_id: BytesN<32>) -> Option<Claim> {
-        env.storage()
+    pub fn get_claim(env: Env, claim_id: BytesN<32>) -> Result<Option<Claim>, Error> {
+        Ok(env.storage()
             .persistent()
-            .get::<BytesN<32>, Claim>(&claim_id)
+            .get::<BytesN<32>, Claim>(&claim_id))
     }
 
-    pub fn get_claims(env: Env) -> Vec<BytesN<32>> {
-        let claims = env
+    pub fn get_claims(env: Env) -> Result<Vec<BytesN<32>>, Error> {
+        Ok(env
             .storage()
             .persistent()
             .get::<Symbol, Vec<BytesN<32>>>(&symbol_short!("claims"))
-            .unwrap_or(Vec::new(&env));
-        claims
+            .unwrap_or(Vec::new(&env)))
     }
 
     pub fn add_claim(
@@ -201,20 +204,19 @@ impl ClaimIssuerContract {
         signature: Bytes,
         data: Bytes,
         uri: Bytes,
-    ) -> BytesN<32> {
-        identity_require_auth(&env, &sender, KeyPurpose::Claim);
-        
+    ) -> Result<BytesN<32>, Error> {
+        identity_require_auth(&env, &sender, KeyPurpose::Claim)?;
+
         let current_contact = env.current_contract_address();
 
         if current_contact != issuer {
             let client = claim_issuer::Client::new(&env, &issuer);
             if client.is_claim_valid(&issuer, &topic, &signature, &data) {
-                panic!("Claim is not valid")
+                return Err(Error::InvalidClaim);
             }
         }
 
         let claim_id = hash_claim(&env, &issuer, &topic);
-        log!(&env, "Adding claim with ID: {:?}", claim_id,);
         let claim: Claim = Claim {
             topic,
             scheme,
@@ -236,17 +238,17 @@ impl ClaimIssuerContract {
         // TODO: Call emitClaimAdded
         log!(&env, "Claim added: {:?}", claim);
 
-        claim_id
+        Ok(claim_id)
     }
 
-    pub fn remove_claim(env: Env, sender: Address, claim_id: BytesN<32>) {
-        identity_require_auth(&env, &sender, KeyPurpose::Claim);
+    pub fn remove_claim(env: Env, sender: Address, claim_id: BytesN<32>) -> Result<(), Error>  {
+        identity_require_auth(&env, &sender, KeyPurpose::Claim)?;
 
         let claim = env
             .storage()
             .persistent()
             .get::<BytesN<32>, Claim>(&claim_id)
-            .expect("Claim not found");
+            .ok_or(Error::ClaimNotFound)?;
 
         env.storage().persistent().remove(&claim_id);
 
@@ -266,29 +268,7 @@ impl ClaimIssuerContract {
 
         // TODO: Call emitClaimRemoved
         log!(&env, "Claim removed: {:?}", claim);
-    }
-
-    pub fn revoke_claim(env: Env, sender: Address, contract: Address, claim_id: BytesN<32>) {
-        identity_require_auth(&env, &sender, KeyPurpose::Management);
-        let client = identity::Client::new(&env, &contract);
-
-        let claim = client.get_claim(&claim_id).expect("Claim not found");
-
-        let revoked_symbol = Symbol::new(&env, "revoked_claims");
-
-        let mut claims = env
-            .storage()
-            .persistent()
-            .get::<Symbol, Vec<Bytes>>(&revoked_symbol)
-            .unwrap_or(Vec::new(&env));
-
-        if claims.contains(&claim.signature) {
-            panic!("Claim already revoked");
-        }
-
-        claims.push_back(claim.signature.clone());
-
-        env.storage().persistent().set(&revoked_symbol, &claims);
+        Ok(())
     }
 
     pub fn is_claim_valid(
@@ -297,7 +277,7 @@ impl ClaimIssuerContract {
         topic: U256,
         signature: Bytes,
         data: Bytes,
-    ) -> bool {
+    ) -> Result<bool, Error> {
         let address_bytes = Bytes::from_val(env, &issuer.to_xdr(&env));
         let topic_bytes = Bytes::from_val(env, &topic.to_xdr(env));
 
@@ -315,11 +295,11 @@ impl ClaimIssuerContract {
         let signature_slice: BytesN<64> = signature
             .slice(..64)
             .try_into()
-            .expect("bytes to have length 64");
+            .map_err(|_| Error::InvalidSignature)?;
 
         let recovery_id = match signature.get(64) {
             Some(v) => (v + 27) as u32,
-            None => panic!("Expected signature to have 65 bytes"),
+            None => return Err(Error::InvalidSignature),
         };
 
         let recovered: BytesN<65> =
@@ -328,10 +308,35 @@ impl ClaimIssuerContract {
         let recovered_addr = Address::from_string_bytes(&recovered.to_xdr(env));
         let hashed_addr = env.crypto().keccak256(&recovered_addr.to_xdr(env));
 
-        key_has_purpose(env, &hashed_addr, KeyPurpose::Claim) && !Self::is_claim_revoked(env, signature)
+        Ok(key_has_purpose(env, &hashed_addr, KeyPurpose::Claim))
     }
 
-    pub fn is_claim_revoked(env: &Env, signature: Bytes) -> bool {
+    pub fn revoke_claim(env: Env, sender: Address, contract: Address, claim_id: BytesN<32>) -> Result<(), Error>{
+        identity_require_auth(&env, &sender, KeyPurpose::Management)?;
+        let client = identity::Client::new(&env, &contract);
+
+        let claim = client.get_claim(&claim_id).ok_or(Error::ClaimNotFound)?;
+
+        let revoked_symbol = Symbol::new(&env, "revoked_claims");
+
+        let mut claims = env
+            .storage()
+            .persistent()
+            .get::<Symbol, Vec<Bytes>>(&revoked_symbol)
+            .unwrap_or(Vec::new(&env));
+
+        if claims.contains(&claim.signature) {
+            return Err(Error::ClaimAlreadyRevoked);
+        }
+
+        claims.push_back(claim.signature.clone());
+
+        env.storage().persistent().set(&revoked_symbol, &claims);
+
+        Ok(())
+    }
+
+    pub fn is_claim_revoked(env: &Env, signature: Bytes) -> Result<bool, Error> {
         let revoked_symbol = Symbol::new(&env, "revoked_claims");
 
         let claims = env
@@ -340,7 +345,7 @@ impl ClaimIssuerContract {
             .get::<Symbol, Vec<Bytes>>(&revoked_symbol)
             .unwrap_or(Vec::new(&env));
 
-        claims.contains(&signature)
+        Ok(claims.contains(&signature))
     }
 }
 
@@ -371,14 +376,15 @@ fn key_has_purpose(env: &Env, key_hash: &BytesN<32>, purpose: KeyPurpose) -> boo
         false
     }
 }
-fn identity_require_auth(env: &Env, sender: &Address, key_type: KeyPurpose) {
+fn identity_require_auth(env: &Env, sender: &Address, key_type: KeyPurpose) -> Result<(), Error> {
     let key_hash = hash_key(env, sender);
 
     if !key_has_purpose(env, &key_hash, key_type) {
-        panic!("Permissions: Sender does not have the required key type");
+        return Err(Error::InsufficientPermissions);
     }
 
     sender.require_auth();
+    Ok(())
 }
 
 mod test;
